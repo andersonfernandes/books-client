ffd9c9299603e22771ca7faa1040413243323699
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-10-19 20:03:38 -0300

    Add access to books api
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ src/components/books/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="book in books" :key="book._id">
+      {{ book.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BooksList',
+  data: () => ({
+    books: [],
+  }),
+  created() {
+    fetch('https://expressaula.herokuapp.com/books')
+      .then(response => response.json())
+      .then(data => this.books = data);
+  }
+}
+</script>
